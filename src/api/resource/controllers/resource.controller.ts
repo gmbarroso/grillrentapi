14671530be2e38f559222b7c857f4f45ee685af2
gmbarroso <barroso.guilherme@gmail.com>
2025-02-23 18:23:56 -0300

    fix: fix permissions to resource calls

--- src/api/resource/controllers/resource.controller.ts
+++ src/api/resource/controllers/resource.controller.ts
@@ -1,54 +1,46 @@
 import { Controller, Post, Body, Logger, Get, Put, Delete, Param, UseGuards, ForbiddenException } from '@nestjs/common';
-import { CreateResourceDto, CreateResourceSchema } from '../dto/create-resource.dto';
-import { UpdateResourceDto, UpdateResourceSchema } from '../dto/update-resource.dto';
+import { CreateResourceDto } from '../dto/create-resource.dto';
+import { UpdateResourceDto } from '../dto/update-resource.dto';
 import { ResourceService } from '../services/resource.service';
-import { JoiValidationPipe } from '../../../shared/pipes/joi-validation.pipe';
 import { JwtAuthGuard } from '../../../shared/auth/guards/jwt-auth.guard';
 import { User } from '../../../shared/auth/decorators/user.decorator';
 import { User as UserEntity, UserRole } from '../../user/entities/user.entity';
 
 @Controller('resources')
-@UseGuards(JwtAuthGuard)
 export class ResourceController {
   private readonly logger = new Logger(ResourceController.name);
 
   constructor(private readonly resourceService: ResourceService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Post()
-  async create(@User() user: UserEntity, @Body(new JoiValidationPipe(CreateResourceSchema)) createResourceDto: CreateResourceDto) {
-    this.logger.log(`User ID: ${user.id} with role: ${user.role} attempting to create resource`);
+  async create(@User() user: UserEntity, @Body() createResourceDto: CreateResourceDto) {
+    this.logger.log(`Creating resource by user ID: ${user.id}`);
     if (user.role !== UserRole.ADMIN) {
       this.logger.warn(`User ID: ${user.id} does not have permission to create resources`);
       throw new ForbiddenException('You do not have permission to create resources');
     }
-    const result = await this.resourceService.create(createResourceDto);
-    this.logger.log(`Resource created successfully by user ID: ${user.id}`);
-    return result;
+    return this.resourceService.create(createResourceDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async findAll(@User() user: UserEntity) {
-    this.logger.log(`User ID: ${user.id} with role: ${user.role} attempting to find all resources`);
-    if (user.role !== UserRole.ADMIN) {
-      this.logger.warn(`User ID: ${user.id} does not have permission to view resources`);
-      throw new ForbiddenException('You do not have permission to view resources');
-    }
+    this.logger.log(`Fetching all resources by user ID: ${user.id}`);
     return this.resourceService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   async findOne(@User() user: UserEntity, @Param('id') id: string) {
-    this.logger.log(`User ID: ${user.id} with role: ${user.role} attempting to find resource ID: ${id}`);
-    if (user.role !== UserRole.ADMIN) {
-      this.logger.warn(`User ID: ${user.id} does not have permission to view resources`);
-      throw new ForbiddenException('You do not have permission to view resources');
-    }
+    this.logger.log(`Fetching resource ID: ${id} by user ID: ${user.id}`);
     return this.resourceService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
-  async update(@User() user: UserEntity, @Param('id') id: string, @Body(new JoiValidationPipe(UpdateResourceSchema)) updateResourceDto: UpdateResourceDto) {
-    this.logger.log(`User ID: ${user.id} with role: ${user.role} attempting to update resource ID: ${id}`);
+  async update(@User() user: UserEntity, @Param('id') id: string, @Body() updateResourceDto: UpdateResourceDto) {
+    this.logger.log(`Updating resource ID: ${id} by user ID: ${user.id}`);
     if (user.role !== UserRole.ADMIN) {
       this.logger.warn(`User ID: ${user.id} does not have permission to update resources`);
       throw new ForbiddenException('You do not have permission to update resources');
@@ -56,15 +48,14 @@ export class ResourceController {
     return this.resourceService.update(id, updateResourceDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   async remove(@User() user: UserEntity, @Param('id') id: string) {
-    this.logger.log(`User ID: ${user.id} with role: ${user.role} attempting to remove resource ID: ${id}`);
+    this.logger.log(`Removing resource ID: ${id} by user ID: ${user.id}`);
     if (user.role !== UserRole.ADMIN) {
-      this.logger.warn(`User ID: ${user.id} does not have permission to delete resources`);
-      throw new ForbiddenException('You do not have permission to delete resources');
+      this.logger.warn(`User ID: ${user.id} does not have permission to remove resources`);
+      throw new ForbiddenException('You do not have permission to remove resources');
     }
-    const result = await this.resourceService.remove(id);
-    this.logger.log(`Resource removed successfully by user ID: ${user.id}`);
-    return result;
+    return this.resourceService.remove(id);
   }
 }
